d08247b7f1e05abe775e6a213962bf0ed0c9a11c
Dev <giangpthe161721@fpt.edu.vn>
2023-06-13 14:05:31 +0700

    Update mui, use-react-hook

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,42 @@
+'use client'
 import React from 'react'
+import { useForm, SubmitHandler, Form } from 'react-hook-form'
 import Image from 'next/image'
 import { LockClosedIcon, EnvelopeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
+import { CheckIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import { Input, InputAdornment, Button, FormControl, InputLabel } from '@mui/material'
 import facebook from '../../assets/Image/Facebook.png'
 import apple from '../../assets/Image/apple.png'
 import google from '../../assets/Image/google.png'
+import { error } from 'console'
 
 type Props = {}
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 const Login = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const [viewPassword, setViewPassword] = React.useState(false)
+
+  const handleViewPassword = () => {
+    setViewPassword(!viewPassword)
+  }
+
   return (
     <>
       <h3 className="text-4xl font-medium my-5">Sign in</h3>
@@ -18,36 +48,51 @@ const Login = (props: Props) => {
           Register here !
         </a>
       </span>
-      <form className="my-8">
-        <div className="border-b-2 border-gray-400 block mb-10 text-sm">
-          <label className="text-gray-400 " htmlFor="femail">
-            Email
-          </label>
+      <form className="my-8" onSubmit={handleSubmit(onSubmit)}>
+        <div className="block mb-10 text-sm">
           <div className="flex ">
-            <EnvelopeIcon className="w-6 mt-2 mr-3" />
-            <input
-              className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950 "
-              type="text"
-              placeholder="Enter your email address"
-              id="femail"
-            />
+            <FormControl variant="standard" className="w-full">
+              <InputLabel htmlFor="femail">Email</InputLabel>
+              <Input
+                {...register('email', { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })}
+                className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950 "
+                placeholder="Enter your email address"
+                id="femail"
+                {...(errors.email ? { error: true } : { color: 'success' })}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <EnvelopeIcon className="w-6" />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </div>
         </div>
-        <div className="border-b-2 border-gray-400 block text-sm">
-          <label className="text-gray-400 " htmlFor="fpassword">
-            Password
-          </label>
+        <div className="block text-sm">
           <div className="flex ">
-            <LockClosedIcon className="w-6 mt-2 mr-3" />
-            <input
-              className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950 "
-              type="password"
-              placeholder="Enter your password"
-              id="fpassword"
-            />
-            <button type="button">
-              <EyeSlashIcon className="w-6 mt-2 text-gray-400" />
-            </button>
+            <FormControl variant="standard" className="w-full">
+              <InputLabel htmlFor="fpassword">Password</InputLabel>
+              <Input
+                {...register('password', { required: true, minLength: 8, maxLength: 20 })}
+                className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950 "
+                type={viewPassword ? 'text' : 'password'}
+                placeholder="Enter your password"
+                id="fpassword"
+                {...(errors.password ? { error: true } : { color: 'success' })}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LockClosedIcon className="w-6" />
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <InputAdornment position="end">
+                    <button type="button" onClick={handleViewPassword}>
+                      <EyeSlashIcon className="w-6 mt-2 text-gray-400" />
+                    </button>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </div>
         </div>
         <div className="flex justify-between mt-3">
@@ -62,12 +107,13 @@ const Login = (props: Props) => {
           </div>
         </div>
         <div className="mt-10">
-          <button
+          <Button
+            variant="contained"
             type="button"
             className="w-full bg-blue-800 rounded-full text-white py-3 active:bg-blue-500 hover:bg-blue-700 transition-all"
           >
             Login
-          </button>
+          </Button>
         </div>
       </form>
       <div className="w-full text-center italic text-gray-500">
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,47 @@
+'use client'
 import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import { EnvelopeIcon, UserIcon, LockClosedIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
+import { Input, InputAdornment, Button, FormControl, InputLabel } from '@mui/material'
 type Props = {}
 
+interface IFormInput {
+  email: string
+  username: string
+  password: string
+  confirmPassword: string
+}
+
+const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
 const SignUp = (props: Props) => {
+  const [viewPassword, setViewPassword] = React.useState(false)
+  const [viewConfirmPassword, setViewConfirmPassword] = React.useState(false)
+  const [email, setEmail] = React.useState('')
+
+  const handleViewPassword = () => {
+    setViewPassword(!viewPassword)
+  }
+
+  const handleViewConfirmPassword = () => {
+    setViewConfirmPassword(!viewConfirmPassword)
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<IFormInput>({
+    mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
   return (
     <>
       <h3 className="text-2xl sm:text-4xl font-medium my-3 md:my-5">Sign up</h3>
@@ -14,76 +53,111 @@ const SignUp = (props: Props) => {
           Login here !
         </a>
       </span>
-      <form className="my-10">
-        <div className="border-b-2 border-gray-400 mb-5 md:mb-8 text-sm">
-          <label className="text-gray-400 " htmlFor="femail">
-            Email
-          </label>
+      <form className="my-10" onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-5 md:mb-8 text-sm">
           <div className="flex ">
-            <EnvelopeIcon className="w-6 mt-2 mr-3" />
-            <input
-              className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
-              type="text"
-              placeholder="Enter your email address"
-              id="femail"
-            />
+            <FormControl variant="standard" className="w-full">
+              <InputLabel htmlFor="femail">Email</InputLabel>
+              <Input
+                {...register('email', { required: true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })}
+                className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
+                type="text"
+                placeholder="Enter your email address"
+                id="femail"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <EnvelopeIcon className="w-6 mt-2 mr-3" />
+                  </InputAdornment>
+                }
+                {...(errors.email ? { error: true } : { color: 'success' })}
+              />
+            </FormControl>
           </div>
         </div>
-        <div className="border-b-2 border-gray-400 text-sm mb-5 md:mb-8">
-          <label className="text-gray-400 " htmlFor="fuser">
-            Username
-          </label>
+        <div className="text-sm mb-5 md:mb-8">
           <div className="flex ">
-            <UserIcon className="w-6 mt-2 mr-3" />
-            <input
-              className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
-              type="password"
-              placeholder="Enter your User name"
-              id="fuser"
-            />
+            <FormControl variant="standard" className="w-full">
+              <InputLabel htmlFor="fuser">Username</InputLabel>
+              <Input
+                {...register('username', { required: true })}
+                className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
+                type="password"
+                placeholder="Enter your User name"
+                id="fuser"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <UserIcon className="w-6 mt-2 mr-3" />
+                  </InputAdornment>
+                }
+                {...(errors.username ? { error: true } : { color: 'success' })}
+              />
+            </FormControl>
           </div>
         </div>
-        <div className="border-b-2 border-gray-400 text-sm mb-5 md:mb-8">
-          <label className="text-gray-400 " htmlFor="fpassword">
-            Password
-          </label>
+        <div className="text-sm mb-5 md:mb-8">
           <div className="flex ">
-            <LockClosedIcon className="w-6 mt-2 mr-3" />
-            <input
-              className=" py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
-              type="password"
-              placeholder="Enter your Password"
-              id="fpassword"
-            />
-            <button type="button">
-              <EyeSlashIcon className="w-6 mt-2 text-gray-400" />
-            </button>
+            <FormControl variant="standard" className="w-full">
+              <InputLabel htmlFor="fpassword">Password</InputLabel>
+              <Input
+                {...register('password', { required: true, minLength: 8, maxLength: 20 })}
+                className="py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950 "
+                type={viewPassword ? 'text' : 'password'}
+                placeholder="Enter your password"
+                id="fpassword"
+                {...(errors.password ? { error: true } : { color: 'success' })}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LockClosedIcon className="w-6 mt-2 mr-3" />
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <InputAdornment position="end">
+                    <button type="button" onClick={handleViewPassword}>
+                      <EyeSlashIcon className="w-6 mt-2 text-gray-400" />
+                    </button>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </div>
         </div>
-        <div className="border-b-2 border-gray-400 text-sm mb-5 md:mb-8">
-          <label className="text-gray-400 " htmlFor="fcpassword">
-            Confirm Password
-          </label>
+        <div className="text-sm mb-5 md:mb-8">
           <div className="flex ">
-            <LockClosedIcon className="w-6 mt-2 mr-3" />
-            <input
-              className=" py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
-              type="password"
-              placeholder="Confirm your Password"
-              id="fcpassword"
-            />
-            <button type="button">
-              <EyeSlashIcon className="w-6 mt-2 text-gray-400" />
-            </button>
+            <FormControl variant="standard" className="w-full">
+              <InputLabel htmlFor="fcpassword">Confirm Password</InputLabel>
+              <Input
+                {...register('confirmPassword', { required: true, minLength: 8, maxLength: 20 })}
+                className=" py-2 focus:outline-none w-full mt-2 focus:border-indigo-950 hover:placeholder:text-gray-500 focus:placeholder:text-gray-400 placeholder:text-indigo-950"
+                type={viewConfirmPassword ? 'text' : 'password'}
+                placeholder="Confirm your Password"
+                id="fcpassword"
+                {...(!errors.confirmPassword &&
+                getValues('password') === getValues('confirmPassword')
+                  ? { color: 'success' }
+                  : { error: true })}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <button type="button" onClick={handleViewConfirmPassword}>
+                      <EyeSlashIcon className="w-6 mt-2 text-gray-400" />
+                    </button>
+                  </InputAdornment>
+                }
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LockClosedIcon className="w-6 mt-2 mr-3" />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </div>
         </div>
         <div className="mt-10 text-center">
-          <button
-            type="button"
+          <Button
+            type="submit"
             className=" w-4/5 md:w-full bg-blue-800 rounded-full text-white py-3 active:bg-blue-500 hover:bg-blue-700 transition-all"
           >
             Register
-          </button>
+          </Button>
         </div>
       </form>
     </>
